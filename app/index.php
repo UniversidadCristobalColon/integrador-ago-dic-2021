7921bf9484ee50e57abb1fe505b87898e4edaf12
Rubén Sánchez <rsanchez@ucc.mx>
2021-11-17 15:06:34 -0600

    Directorios

--- app/index.php
+++ app/index.php
@@ -16,6 +16,7 @@
 
     <!-- Custom styles for this template-->
     <link href="../css/sb-admin.css" rel="stylesheet">
+    <link href="../img/favicon.jpg" rel="shortcut icon" type="image/png"/>
 
 </head>
 
@@ -23,25 +24,26 @@
 
 <div class="container">
     <div class="card card-login mx-auto mt-5">
-        <div class="card-header">Bienvenido</div>
+        <div class="card-header">¡Bienvenido!</div>
         <div class="card-body">
             <form>
                 <div class="form-group">
                     <div class="form-label-group">
-                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address"
+                        <input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico"
                                required="required" autofocus="autofocus">
-                        <label for="inputEmail">Email address</label>
+                        <label for="inputEmail">Correo electrónico</label>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="form-label-group">
-                        <input type="password" id="inputPassword" class="form-control" placeholder="Password"
+                        <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña"
                                required="required">
-                        <label for="inputPassword">Password</label>
+                        <label for="inputPassword">Contraseña</label>
                     </div>
                 </div>
 
                 <a class="btn btn-primary btn-block" href="index.php">Ingresar</a>
+                <a class="btn btn-secondary btn-block" href="registrar.php">¿No tiene una cuenta? Regístrese aquí</a>
             </form>
             <div class="text-center">
                 <a class="d-block small mt-3" href="recuperar.php">¿Olvidó su contraseña?</a>
