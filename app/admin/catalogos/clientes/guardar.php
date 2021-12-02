1e78ff9b1a79f0d1f890f08de62dddc9a8da99d8
Jazael Garcia <jazael.garcia28@gmail.com>
2021-12-01 19:51:57 -0600

    Guardado de clientes

--- app/admin/catalogos/clientes/guardar.php
+++ app/admin/catalogos/clientes/guardar.php
@@ -1,9 +1,41 @@
 <?php
 require '../../../../config/db.php';
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+$celular = $_POST['celular'];
+$telefono = $_POST['telefono'];
 
-$query = "INSERT INTO clientes (id, nombre, apellidos, email, celular, telefono, creacion) 
-VALUES (null,'Jazael','Garcia','jaza@gmail.com','2294857203','394829191',NOW())";
+//facturacion
+$razon = $_POST['razon'];
+$rfc = $_POST['rfc'];
+$email1 = $_POST['email1'];
+$email2 = $_POST['email2'];
+$codigopostal = $_POST['cp'];
+$calle = $_POST['calle'];
+$entrecalles = $_POST['entrecalles'];
+$numexterior = $_POST['numext'];
+$numinterior = $_POST['numint'];
+$colonia = $_POST['colonia'];
+$localidad = $_POST['localidad'];
+$municipio = $_POST['municipio'];
+$estado = $_POST['estado'];
+$referencia = $_POST['referencia'];
+
+
+
+$query = "INSERT INTO clientes (id, nombre, apellidos, email, celular, telefono, creacion)
+VALUES (null,'$nombre','$apellidos','$email1','$celular','$telefono',NOW())";
+
+$query2 = "INSERT INTO fiscales (id, id_cliente, rfc, razon, calle, num_exterior, 
+num_interior, colonia, cp, localidad, municipio, id_estado, email1, email2, creacion, status)
+VALUES (null, 1, '$rfc', '$razon', '$calle', '$numexterior', '$numinterior', '$colonia', 
+'$codigopostal', '$localidad', '$municipio', '1', '$email1', '$email2', NOW(), 'A')";
 
 $resultado = mysqli_query($conexion, $query);
+$resultado2 = mysqli_query($conexion, $query2);
 
-var_dump($resultado);
+if($resultado == true && $resultado2 == true){
+    header('location: index.php');
+}else{
+    echo mysqli_errno($conexion);
+}
