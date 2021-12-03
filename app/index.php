d87df0e2347b58103fa278c83a2fb8b521b98520
carlosshm <201860077@ucc.mx>
2021-12-03 13:09:46 -0600

    Login terminado

--- app/index.php
+++ app/index.php
@@ -1,5 +1,6 @@
 <?php
 session_start()
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -29,40 +30,40 @@ session_start()
     <div class="card card-login mx-auto mt-5">
         <div class="card-header">¡Bienvenido!</div>
         <div class="card-body">
-        <!--Empieza el form-->
+            <!--Empieza el form-->
             <form action="login.php" method="POST">
                 <div class="form-group">
                     <div class="form-label-group">
-                        <input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico"
+                        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Correo electrónico"
                                required="required" autofocus="autofocus">
                         <label for="inputEmail">Correo electrónico</label>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="form-label-group">
-                        <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña"
+                        <input type="password" name="pass" id="inputPassword" class="form-control" placeholder="Contraseña"
                                required="required">
                         <label for="inputPassword">Contraseña</label>
                     </div>
                 </div>
 
-                <a class="btn btn-primary btn-block" href="index.php">Ingresar</a>
+                <button class="btn btn-primary btn-block"  type="submit">Ingresar</button>
                 <a class="btn btn-secondary btn-block" href="registrar.php">¿No tiene una cuenta? Regístrese aquí</a>
-            <!--Mio-->
-                <?php 
-                    if(isset($_SESSION['login_error'])==true && $_SESSION['login_error']==1){
-                        $_SESSION['login_error']=0;
-                ?>
-                <div class="row mt-3">
-                    <div class="col-md-6">
-                        <div class="alert alert-danger">Usuario o contraseña incorrecta</div>
+                <!--Alerta error de datos-->
+                <?php
+                if(isset($_SESSION['login_error'])==true && $_SESSION['login_error']==1){
+                    $_SESSION['login_error']=0;
+                    ?>
+                    <div class="row mt-3">
+                        <div class="col-md-12">
+                            <div class="alert alert-danger">Usuario o contraseña incorrecta</div>
+                        </div>
                     </div>
-                </div>
 
-            <?php
+                    <?php
                 }
-            ?>
-            <!--Mio-->   
+                ?>
+                <!--Termina alerta error de datos-->
             </form>
             <div class="text-center">
                 <a class="d-block small mt-3" href="recuperar.php">¿Olvidó su contraseña?</a>
---
+++ app/main.php
@@ -0,0 +1,56 @@
+<?php
+require_once '../config/global.php';
+
+define('RUTA_INCLUDE', '../'); //ajustar a necesidad
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo PAGE_TITLE ?></title>
+
+    <?php getTopIncludes(RUTA_INCLUDE ) ?>
+</head>
+
+<body id="page-top">
+
+<?php getNavbar() ?>
+
+<div id="wrapper">
+
+    <?php getSidebar() ?>
+
+    <div id="content-wrapper">
+
+        <!-- /.container-fluid -->
+
+        <div class="container">
+            <h3>Bienvenido <?php echo $nombre_usuario." ".$apellidos_usuario?></h3>
+        </div>
+        <!-- /.container -->
+
+        <?php getFooter() ?>
+
+    </div>
+    <!-- /.content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<?php getModalLogout() ?>
+
+<?php getBottomIncudes( RUTA_INCLUDE ) ?>
+</body>
+
+</html>
