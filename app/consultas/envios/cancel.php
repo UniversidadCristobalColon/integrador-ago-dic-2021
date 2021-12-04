55b4e1c704610e348c021f61384464384d0e53da
TiefVoid <placeholder.prod@outlook.com>
2021-12-03 18:57:51 -0600

    envio.php ahora es index.php por cosa del redireccionamiento del menu

--- app/consultas/envios/cancel.php
+++ app/consultas/envios/cancel.php
@@ -8,7 +8,7 @@ $idEnv = $_POST['id'];
 $sql = "update envios set razon_cancela=$razon,comment_cancela=$comment,seguimiento='X',fecha_cancela=NOW(),actualizacion=NOW() where id=$idEnv";
 $resultado = mysqli_query($conexion, $sql);
 if($resultado){
-    header('location: envio.php');
+    header('location: index.php');
 } else {
     echo mysqli_error($conexion);
 }
