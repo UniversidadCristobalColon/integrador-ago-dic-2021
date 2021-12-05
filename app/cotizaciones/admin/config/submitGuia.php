3dfb3f3733146cd38b54feaf633eb444397dd093
Luis Núñez <njluisangel@gmail.com>
2021-12-05 15:08:08 -0600

    fix mensajes de status en pantalla cotizaciones usuario

--- app/cotizaciones/admin/config/submitGuia.php
+++ app/cotizaciones/admin/config/submitGuia.php
@@ -7,6 +7,7 @@ $guia = $_POST['guia'];
 $sqlUpdate = "UPDATE cotizaciones SET guia='$guia', status='3' WHERE id_cotizacion='$id_cotizacion'";
 $resultado = mysqli_query($conexion, $sqlUpdate);
 if ($resultado == true) {
+    // nuevoEnvio($id_cotizacion);
     header('location: ../manage.php?id=' . $id_cotizacion);
 } else {
     echo mysqli_error($conexion);
