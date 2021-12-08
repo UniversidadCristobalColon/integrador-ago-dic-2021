3fb5c2f04f538aec6dc5916357f5a2824bedfab5
Alex Gutierrez <201860293@ucc.mx>
2021-12-08 15:31:52 -0500

    registro t

--- app/models/usuarios.php
+++ app/models/usuarios.php
@@ -20,9 +20,7 @@ class usuarios{
       $query ="INSERT INTO usuarios (user,password,id_perfil,creacion,id_cliente) values ( '$this->user','".password_hash($this->password,PASSWORD_DEFAULT)."', 2, NOW(),'$this->id_perfil');";
       $stmt =  $conexion->prepare($query);
       $respuesta = $stmt->execute();
-      $query = "INSERT INTO fiscales (id,id_cliente,rfc,razon,calle,num_exterior,num_interior,cp,localidad,municipio,id_estado,email1,email2,creacion,actualizacion,status,entrecalles,referencia) values (null,$this->user,'','','','','','','','','',null,'','', NOW(),null,'A','','')";
-    $stmt =  $conexion->prepare($query);
-    $respuesta = $stmt->execute();
+
       return $respuesta;
 
    }	
