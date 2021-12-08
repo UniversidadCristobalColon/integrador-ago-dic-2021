b7f85f6b639aee8a456f737817542929673cd7f4
Bangsito <201860209@ucc.mx>
2021-12-08 11:48:59 -0600

    Almost Done

--- app/consultas/cotizaciones/cotizaciones_filtros_nuevo.php
+++ app/consultas/cotizaciones/cotizaciones_filtros_nuevo.php
@@ -142,7 +142,6 @@ $consulta = $conexion->query($query);
 
 </div>
 <!-- /#wrapper -->
-
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
